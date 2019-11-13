e00f05d1d223d11b537881e0dca8cdf4fa518d2b
Michael Calkins <m.calkins576@gmail.com>
2019-11-13 17:52:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,6 +17,6 @@ class CashRegister
   def apply_discount
     emp_dis = (discount/100.0)
     @total = @total - (@total*emp_dis)
-    puts 
+    puts "After the discount, the total comes to $#{@total}"
   end
 end
